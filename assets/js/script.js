ab1199b4078fbf2d0f9591a8a2564c7e14d9537e
Adam Sweeney <amsweene@gmail.com>
2020-08-24 18:32:06 -0400

    condensing js and adding foundation framework links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+var APIKey = "KmBywIxjpaANpeIoitPkPtCDXEIZZxDJ";
+
+    
+    var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/weather?=" + "q=Charlotte,NorthCarolina&appid=" + APIKey;
+
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      
+      .then(function(response) {
+
+        
+       
+      });
+
+
 function init() {
     new google.maps.Map(document.getElementById('map'), {
       center: { lat: 35.2271, lng: -80.8431 },
