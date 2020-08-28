497808bf775ba299405da2665cfebd3ab9edb310
camilo alvarez <camiloalvarez88@gmail.com>
2020-08-28 15:54:05 -0400

    my attempt for the weather api, my api keys stoped working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,59 @@
-var APIKey = "KmBywIxjpaANpeIoitPkPtCDXEIZZxDJ";
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temp-value p");
+const descElement = document.querySelector(".temp-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
 
-    
-    var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/weather?=" + "q=Charlotte,NorthCarolina&appid=" + APIKey;
+const weather = {};
 
-    
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      
-      .then(function(response) {
-
-        
-       
-      });
-
-
-function init() {
-    new google.maps.Map(document.getElementById('map'), {
-      center: { lat: 35.2271, lng: -80.8431 },
-      zoom: 10
-    });
-  }
-
-  //camilo
-
-var listOfCities = []
-var APIKey = "4f148c9563cb2a85404910d3ad39b157";
-var city
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey + "&units=imperial";
-
-
-$.ajax({url: queryURL, method: "GET"})
-.then(function(response) {
-    console.log(queryURL);
-    console.log(response);
-});
-
-$("#run-search").on("click", function () {
-   city= $("#search-term").val()
-    displayWeather()
-})
-
-$(".search-data").html("<h3>Forecast</h3>")
-
-$("#clear-search").on("click", function (){
-    localStorage.clear("cities")
-    listOfCities = []
-    $(".buttons-view").empty()
-    location.reload()
-    })
-    
-
-function saveCities() {
-    localStorage.setItem("cities", JSON.stringify(listOfCities));
+weather.temperature = {
+  unit : "celsius"
 }
-//=======
-  
-   
-  var APIKey = "KmBywIxjpaANpeIoitPkPtCDXEIZZxDJ";
+const KELVIN =273;
+const key = "4385c4f29ff50ae3190f2e74d27619ba";
 
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/weather?=" + "q=Charlotte,NorthCarolina&appid=" + APIKey;
+if(`geolocation` in navigator){
+  navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+  notificationElement.getElementsByClassName.display = "block";
+  notificationElement.innerHTML = "<p>Browser dosent support geolocation</p>";
+}
 
-    // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // We store all of the retrieved data inside of an object called "response"
-      .then(function(response) {
+function setPosition(position){
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
 
-        // Log the queryURL
-        console.log(queryURL);
+  getWeather(latitude,longitude);
+}
 
-        // Log the resulting object
-        console.log(response);
+function showError(error){
+  notificationElement.style.display = "block";
+  notificationElement.innerHTML =  `<p> ${error.message} </p>`;
+}
 
-        // Transfer content to HTML
-        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        
-        // Convert the temp to fahrenheit
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+function getWeather(latitude, longitude){
+  let api = `https://api.openweathermap.org/data/2.5/weather?
+  lat=${latitude}&lon=${longitude}&appid=${key}`;
+  console.log(api);
+  fetch(api)
+  .then(function(response){
+    let data = response.json();
+    return data;
+  })
+  .then(function(data){
+    weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+    weather.description = data.weather[0].icon;
+    weather.city = data.name;
+    weather.country = data.sys.country;
+  })
+  .then(function(){
+    displayWeather();
+  })
+}
 
-        // add temp content to html
-        $(".temp").text("Temperature (K) " + response.main.temp);
-        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
-      });
-//master
+function displayWeather(){
+  iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+  tempElement.innerHTML = `${weather.temperature.value}°<span>F</span>`;
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML =  `${weather.city}, ${weather.counrty}`;
+}
