ea884aee010e32487cdd7ba76f20e8918d5d365e
Adam Sweeney <amsweene@gmail.com>
2020-08-29 11:15:54 -0400

    css and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,32 @@
-var APIKey = "KmBywIxjpaANpeIoitPkPtCDXEIZZxDJ";
-
-    
-    var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/weather?=" + "q=Charlotte,NorthCarolina&appid=" + APIKey;
-
-    
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      
-      .then(function(response) {
-
-        
-       
+const weatherkey = '4f148c9563cb2a85404910d3ad39b157';
+const mtbkey = '200883403-ada2291e9279c2012a03db0c4517b4ff';
+const mapkey = 'AIzaSyAAnMgdCdZHfBYWb1ASE4k7mQrvSwKK3Es';
+  
+  function weatherBallon( cityID ) {
+      fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + weatherkey)  
+      .then(function(resp) { return resp.json() })
+      .then(function(data) {
+          drawWeather(data);
+      })
+      .catch(function() {
       });
+  }
+  function drawWeather( d ) {
+    var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+    var description = d.weather[0].description; 
+      
+      document.getElementById('description').innerHTML = description;
+      document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
+  }
+  window.onload = function() {
+      weatherBallon( 4460243 );
+  }; 
 
-
-function init() {
-    new google.maps.Map(document.getElementById('map'), {
+  function init() {
+    map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: 35.2271, lng: -80.8431 },
       zoom: 10
     });
+    const bikeLayer = new google.maps.BicyclingLayer();
+    bikeLayer.setMap(map);
   }
-
-  //camilo
-
-var listOfCities = []
-var APIKey = "4f148c9563cb2a85404910d3ad39b157";
-var city
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey + "&units=imperial";
-
-
-$.ajax({url: queryURL, method: "GET"})
-.then(function(response) {
-    console.log(queryURL);
-    console.log(response);
-});
-
-$("#run-search").on("click", function () {
-   city= $("#search-term").val()
-    displayWeather()
-})
-
-$(".search-data").html("<h3>Forecast</h3>")
-
-$("#clear-search").on("click", function (){
-    localStorage.clear("cities")
-    listOfCities = []
-    $(".buttons-view").empty()
-    location.reload()
-    })
-    
-
-function saveCities() {
-    localStorage.setItem("cities", JSON.stringify(listOfCities));
-}
-//=======
-  
-   
-  var APIKey = "KmBywIxjpaANpeIoitPkPtCDXEIZZxDJ";
-
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/weather?=" + "q=Charlotte,NorthCarolina&appid=" + APIKey;
-
-    // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // We store all of the retrieved data inside of an object called "response"
-      .then(function(response) {
-
-        // Log the queryURL
-        console.log(queryURL);
-
-        // Log the resulting object
-        console.log(response);
-
-        // Transfer content to HTML
-        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        
-        // Convert the temp to fahrenheit
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
-        // add temp content to html
-        $(".temp").text("Temperature (K) " + response.main.temp);
-        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
-      });
-//master
